058dad3f7848f7ab022520d182fc28d495aee8b8
Warren Fletcher <warren@fletch.band>
2022-06-08 13:41:36 +0200

    Added Ability to read github repos

--- watcher.py
+++ watcher.py
@@ -1,6 +1,12 @@
 from github import Github
-
+import json
 # First create a Github instance:
 
+config = None
+with open('secrets.json', 'r') as json_file:
+	config = json.load(json_file)
+
 # using an access token
-g = Github("access_token")
+g = Github(config["access_token"])
+for repo in g.get_user().get_repos():
+    print(repo.name)
